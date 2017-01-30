b25c15be5bfe882589686db3f984972ecf1f3942
PawelSpoon <pawel@ich-habe-fertig.com>
2017-01-30 22:02:12 +0100

    Create SailKick.ts

---
+++ translations/SailKick.ts
@@ -0,0 +1,234 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>SailKick</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>EditEntryDialog</name>
+    <message>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Discard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Set title (mandatory)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Comment</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Set comment</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>SongKick Id</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Set SongKick Id</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FileSystemDialog</name>
+    <message>
+        <source>Problem with SD card</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>SD cards with multiple partitions are not supported.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hide system files</source>
+        <extracomment>Used in file browser to hide the system files</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show system files</source>
+        <extracomment>Used in file browser to show the hidden system files</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hide file filter</source>
+        <extracomment>Used in file browser to hide the file name filter</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show file filter</source>
+        <extracomment>Used in file browser to show the file name filter</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <extracomment>As in select file</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>New file name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Enter new file name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>File filter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Set file filter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Back</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Device Memory</source>
+        <extracomment>&quot;Device Memory&quot; is used in the file browser and means all files which are saved under home folder of the user</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>SD Card</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Android Storage</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>HelpMainPage</name>
+    <message>
+        <source>This is a native SongKick application.&lt;br&gt;&lt;br&gt;&lt;h2&gt;Main Page&lt;/h2&gt;
+This page shows the upcomming events for selected artists, venues, metro areas. &lt;br&gt;
+Click on an item will open the details page [not implemented]
+PressAndHold on an item will open the event on songkick.com in browser [not implemented]&lt;br&gt;
+PullDownMenu Refresh will refresh it&apos;s contect.
+PullDownMenu Settings will open settings page.
+PushUpMenu Top will navigate to first item in list.
+PushUpMenu Help did open this help page.
+PushUpMenu About will open general Info page [not implemented]
+&lt;h2&gt;Settings Page&lt;/h2&gt;
+This page allows you to define who you want to track. &lt;br&gt;
+Use the various PushUpMenu&apos;s to add an artist / venue or metro area.
+Click on an item opens the edit page where you can change some details.
+PushAndHold on an item allows you to delete a single item.
+&lt;h2&gt;&lt;/h2&gt;
+A browse mechanism is currently not implemented. You have to define the name and the SongKick id manually.
+For this, open SongKick.com, search for the artist / venue / metro area and open it. The inspect the url to get the id.
+i.e.:
+https://www.songkick.com/artists/253846-radiohead
+&lt;br&gt;
+You will need to provide your username. this is needed to allow usage of SongKick API. </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>HelpPage</name>
+    <message>
+        <source>a native SongKick client</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Upcomming Events</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Top</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Add an artist, metro area or venue to your tracking list in Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PasswordFieldCombo</name>
+    <message>
+        <source>Enter master password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Master password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Confirm master password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Master password confirmed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Confirm password</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Delete all</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Add artist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Add venue</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Add metro area</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
